de76e8b94f1ff4e51f57740087304ee35c3a6892
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-04 13:08:19 -0500

    commit tree 7b154a56eb7400b6e0a9b9d7fbdfc8ea5d86a972

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someFeet){
+  if (someFeet < 400) {
+    return 'This one is on me!'
+  }
+  else if (someFeet < 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (someFeet <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (someFeet > 2500) {
+    return "No can do."
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  if (someCity === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else if (someCity !== 'NYC'){
+    return 'No go.'
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipDescription){
+  if (tipDescription === "generous") {
+    return 'Thank you so much.'
+  } 
+  else if (tipDescription === "not as generous"){
+    return 'Thank you.'
+  }
+  else {
+    return 'Bye.'
+  }
 }
